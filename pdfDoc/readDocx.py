8ebca5e3b492047259633fef0c703252559dc96c
zhou-baopeng <zhou.baopeng@navercorp.com>
2020-03-17 17:10:20 +0800

    pdf and docs

---
+++ pdfDoc/readDocx.py
@@ -0,0 +1,11 @@
+#! python
+
+import docx
+
+
+def getText(filename):
+    doc = docx.Document(filename)
+    fullText = []
+    for para in doc.paragraphs:
+        fullText.append(para.text)
+    return '\n'.join(fullText)
